3bdfeafe141ccd973870a6fc538e8366e74ed809
Best_Jungle <763941163@qq.com>
2017-02-13 14:17:44 +0800

    this is a back-up for remote server

---
+++ wp-content/themes/restaurant-pt/framework/partials/footer-partials/copyright.php
@@ -0,0 +1,15 @@
+<?php ?>
+
+<section id="rpt-copyright" class="full-section-10">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+            <p>
+                <?php _e('created by','restaurant-pt'); ?>
+                <a rel="nofollow" href="<?php echo esc_url(__('http://ketchupthemes.com/alex-itsios/','restaurant-pt')); ?>"><?php printf(__('Ketchupthemes.com','restaurant-pt')); ?> </a>
+                - <?php bloginfo('name'); ?>
+            </p>
+            </div>
+        </div>
+    </div>
+</section>
